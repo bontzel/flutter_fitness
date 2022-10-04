afa984c513ab794aac28bd1abec3ecb4df715cec
Bonta Cosmin <bontzel@gmail.com>
2022-10-04 15:40:18 +0300

    move undo deletion in workouts

--- lib/workouts/view/workouts_list.dart
+++ lib/workouts/view/workouts_list.dart
@@ -8,27 +8,54 @@ class WorkoutsListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<WorkoutsBloc, WorkoutsState>(
-      builder: (context, state) {
-        switch (state.status) {
-          case WorkoutsStatus.initial:
-            return const Center(child: Text('Initial'));
-          case WorkoutsStatus.failure:
-            return const Center(child: Text('failed to fetch workouts'));
-          case WorkoutsStatus.success:
-            if (state.workouts.isEmpty) {
-              return const Center(child: Text('no workouts'));
-            }
-            return ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return WorkoutListItem(workout: state.workouts[index]);
-              },
-              itemCount: state.workouts.length,
-            );
-          case WorkoutsStatus.loading:
-            return const Center(child: CircularProgressIndicator());
-        }
+    return BlocListener<WorkoutsBloc, WorkoutsState>(
+      listenWhen: (previous, current) =>
+          previous.lastDeletedWorkout != current.lastDeletedWorkout &&
+          current.lastDeletedWorkout != null,
+      listener: (context, state) {
+        final deletedWorkout = state.lastDeletedWorkout!;
+        final messenger = ScaffoldMessenger.of(context);
+        messenger
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            SnackBar(
+              content: Text(
+                '${deletedWorkout.name} has been deleted',
+              ),
+              action: SnackBarAction(
+                label: 'Undo',
+                onPressed: () {
+                  messenger.hideCurrentSnackBar();
+                  context
+                      .read<WorkoutsBloc>()
+                      .add(const WorkoutsUndoDeletionRequested());
+                },
+              ),
+            ),
+          );
       },
+      child: BlocBuilder<WorkoutsBloc, WorkoutsState>(
+        builder: (context, state) {
+          switch (state.status) {
+            case WorkoutsStatus.initial:
+              return const Center(child: Text('Initial'));
+            case WorkoutsStatus.failure:
+              return const Center(child: Text('failed to fetch workouts'));
+            case WorkoutsStatus.success:
+              if (state.workouts.isEmpty) {
+                return const Center(child: Text('no workouts'));
+              }
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return WorkoutListItem(workout: state.workouts[index]);
+                },
+                itemCount: state.workouts.length,
+              );
+            case WorkoutsStatus.loading:
+              return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
